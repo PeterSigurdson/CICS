5a0c920d9c06e60fa8c293112bdd700491fc8ba9
peter sigurdson <peter@petersigurdson.net>
2020-03-02 14:57:47 -0500

    3 pm  commit

--- CICS/Program.cs
+++ CICS/Program.cs
@@ -43,6 +43,25 @@ namespace CICS
             // use substrings to breaks it apart around the commas
             dr.FULLADDRESS = "";
 
+            // sample code to walk over a string
+
+            string str;
+            int l = 0;
+
+            Console.Write("\n\nprint individual characters of string in reverse order :\n");
+            Console.Write("------------------------------------------------------\n");
+            Console.Write("Input the string : ");
+            str = "MY name is Peanut I am a Bichon";
+
+            l = str.Length - 1;
+            Console.Write("The characters of the string in reverse are : \n");
+            while (l >= 0)
+            {
+                Console.Write("{0} ", str[l]);
+                l--;
+            }
+            Console.Write("\n\n");
+
         }
     }
 
